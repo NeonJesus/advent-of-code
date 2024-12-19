5b843a7cd5f9dd6ebbf8fbbe366ef40b36cc6cdc
Paul Walters <noirguy22@gmail.com>
2024-12-18 22:35:07 -0800

    removes whitespace and print vector values

--- 2024/day-1-historian-hysteria/src/main.rs
+++ 2024/day-1-historian-hysteria/src/main.rs
@@ -1,8 +1,13 @@
-use std::env;
 use std::fs;
 
 fn main() {
-    let contents = fs::read_to_string("./input.txt")
+    let contents = fs::read_to_string("src/input.txt")
         .expect("Should have been able to read the file");
-    println!("With text:\n{contents}");
+
+    // let mut left_list: Vec<&str> = Vec::new();
+    let clean_list = contents.split_whitespace();
+    let left_list: Vec<&str> = clean_list.collect();
+    for string in &left_list {
+        println!("With text:\n{string}");
+    }
 }
