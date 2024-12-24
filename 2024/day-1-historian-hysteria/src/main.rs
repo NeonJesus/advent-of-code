50a30e7133e9003c838ee6d567cd505dade410e3
Paul Walters <noirguy22@gmail.com>
2024-12-23 18:19:50 -0800

    break full list into left and right columns

--- 2024/day-1-historian-hysteria/src/main.rs
+++ 2024/day-1-historian-hysteria/src/main.rs
@@ -1,13 +1,23 @@
+
 use std::fs;
 
 fn main() {
     let contents = fs::read_to_string("src/input.txt")
         .expect("Should have been able to read the file");
 
-    // let mut left_list: Vec<&str> = Vec::new();
+    let mut left_list: Vec<&str> = Vec::new();
+    let mut right_list: Vec<&str> = Vec::new();
     let clean_list = contents.split_whitespace();
-    let left_list: Vec<&str> = clean_list.collect();
-    for string in &left_list {
-        println!("With text:\n{string}");
+    let full_list: Vec<&str> = clean_list.collect();
+    for string in full_list.iter().step_by(2) {
+        left_list.push(string);
     }
+
+    for string in full_list.iter().skip(1).step_by(2) {
+        right_list.push(string);
+    }
+
+    assert_eq!(left_list.len(), right_list.len());
+    println!("Left Vector: {:?}", left_list);
+    println!("Right Vector: {:?}", right_list);
 }
