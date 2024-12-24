33bf76983d8ffc90b3f4cdf71b67d9bfed9f44e9
Paul Walters <noirguy22@gmail.com>
2024-12-23 18:42:48 -0800

    more accurate var names, sort vactor values

--- 2024/day-1-historian-hysteria/src/main.rs
+++ 2024/day-1-historian-hysteria/src/main.rs
@@ -5,19 +5,25 @@ fn main() {
     let contents = fs::read_to_string("src/input.txt")
         .expect("Should have been able to read the file");
 
-    let mut left_list: Vec<&str> = Vec::new();
-    let mut right_list: Vec<&str> = Vec::new();
+    let mut left_column: Vec<&str> = Vec::new();
+    let mut right_column: Vec<&str> = Vec::new();
     let clean_list = contents.split_whitespace();
     let full_list: Vec<&str> = clean_list.collect();
     for string in full_list.iter().step_by(2) {
-        left_list.push(string);
+        left_column.push(string);
     }
 
     for string in full_list.iter().skip(1).step_by(2) {
-        right_list.push(string);
+        right_column.push(string);
     }
 
-    assert_eq!(left_list.len(), right_list.len());
-    println!("Left Vector: {:?}", left_list);
-    println!("Right Vector: {:?}", right_list);
+    assert_eq!(left_column.len(), right_column.len());
+    // println!("Left Vector: {:?}", left_column);
+    // println!("Right Vector: {:?}", right_column);
+    left_column.sort();
+    right_column.sort();
+
+    for (index, val) in left_column.iter().enumerate() {
+        println!("Index : {index} Value: {val}");
+    }
 }
